453416056149b54e5b648bcf385900c9ca47c172
IkkiKuuro <iranildocarlosd@gmail.com>
2025-05-21 08:29:49 -0300

    Rotas simples feitas

--- API/server.js
+++ API/server.js
@@ -1,14 +1,19 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
 const users = []
 
 app.post('/usuarios', (req, res) => {
-    
+    users.push(req.body)
+
+    res.send('Ok, aqui deu bom')
 })
+
+
 app.get('/usuarios', (req, res) => {
-    res.send('Ok, deu bom')
+    res.json(users)
 })
 
 app.listen(3000)
